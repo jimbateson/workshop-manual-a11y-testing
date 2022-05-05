ef9d815086bd76775148c78e46c5793c31f20193
James Bateson <james.bateson@codecomputerlove.com>
2022-05-05 16:15:55 +0100

    fix: fixing some of the issues along with the workshop content. Messed up some styles along the way though.

--- js/menu.js
+++ js/menu.js
@@ -1,32 +1,45 @@
 function load() {
-    const menuItems = Array.from(document.querySelectorAll('.megamenu-navitem'))
-    const submenus = Array.from(document.querySelectorAll('.megamenu-section'))
+	const menuItems = Array.from(
+		document.querySelectorAll('.megamenu-navitem')
+	);
+	const submenus = Array.from(document.querySelectorAll('.megamenu-section'));
 
-    function resetSubmenus(event) {
-        var activeSubmenu = document.querySelector('.megamenu-section.active')
-        if (activeSubmenu && !activeSubmenu.contains(event.target)) {
-            activeSubmenu.classList.remove('active')
-        }
-    }
+	function resetSubmenus(event) {
+		var activeSubmenu = document.querySelector('.megamenu-section.active');
+		if (activeSubmenu && !activeSubmenu.contains(event.target)) {
+			activeSubmenu.classList.remove('active');
+		}
+	}
 
-    menuItems.forEach((menuItem, index) => {
-        let parentElement = menuItem.parentNode
-        menuItem.addEventListener('click', function(event) {
-            console.log(menuItem)
-            resetSubmenus(event)
+	menuItems.forEach((menuItem, index) => {
+		let parentElement = menuItem.parentNode;
+		menuItem.addEventListener('click', function (event) {
+			console.log(menuItem);
+			resetSubmenus(event);
 
-            // Add active menu for the item clicked
-            if(!parentElement.classList.contains('active')) {
-                parentElement.classList.add('active')
+			// Add active menu for the item clicked
+			if (!parentElement.classList.contains('active')) {
+				parentElement.classList.add('active');
+			} else {
+				parentElement.classList.remove('active');
+			}
+		});
+	});
 
-            } else {
-                parentElement.classList.remove('active')
-            }
-        })
-    })
+	submenus.forEach((submenu, index) => {
+		submenu.addEventListener('keyup', event => {
+			if (
+				event.key === 'Escape' &&
+				submenu.classList.contains('active')
+			) {
+				submenu.classList.remove('active');
+				submenu.parentNode.focus();
+			}
+		});
+	});
 
-    document.addEventListener('click', (event) => {
-        resetSubmenus(event)
-    })
+	document.addEventListener('click', event => {
+		resetSubmenus(event);
+	});
 }
-document.addEventListener('DOMContentLoaded', load)
+document.addEventListener('DOMContentLoaded', load);
